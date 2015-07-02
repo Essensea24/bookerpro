76f2bef6c850c7090a855fea88acc1c99e3accf7
Megan Kakitsubo <megankaki@gmail.com>
2015-07-01 19:53:24 -0700

    add flash message for create and display in the applicaiton layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Account successfully created."
       redirect_to :index
     else
       render :new
