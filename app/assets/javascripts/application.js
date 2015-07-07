c8a6f5649afacfe818246a5ff75ddcada1f062e6
Megan Kakitsubo <megankaki@gmail.com>
2015-07-06 17:15:35 -0700

    add initialize method for google maps in app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,12 @@
 //= require moment
 //= require_tree .
 
-
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(34.0500,-118.2500),
+    zoom:5,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
