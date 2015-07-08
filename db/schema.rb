915313aca1ce93bbf3766f14a1c9526d2c4ca367
Yo Wakita <wakita@gmail.com>
2015-07-07 18:53:32 -0700

    Changes sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150707235822) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "remote_hotels", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
