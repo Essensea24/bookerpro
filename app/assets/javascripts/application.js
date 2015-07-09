f3a0c62376ee916b3da008412eee36672b8296b2
Yo Wakita <wakita@gmail.com>
2015-07-08 19:14:22 -0700

    Adds html box overlay next to google map marker.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,17 +35,58 @@ function initialize_my_map() {
 
 	  	var bounds = new google.maps.LatLngBounds()
 		
+
 		var infowindow = new google.maps.InfoWindow();
-	 
+	 	CustomMarker.prototype.draw = function() {
+	
+	var self = this;
+	
+	var div = this.div;
+	
+	if (!div) {
+	
+		div = this.div = document.createElement('div');
+		
+		div.className = 'marker';
+		
+		div.style.position = 'absolute';
+		div.style.cursor = 'pointer';
+		div.style.width = '20px';
+		div.style.height = '20px';
+		div.style.background = 'blue';
+		
+		if (typeof(self.args.marker_id) !== 'undefined') {
+			div.dataset.marker_id = self.args.marker_id;
+		}
+		
+		google.maps.event.addDomListener(div, "click", function(event) {			
+			google.maps.event.trigger(self, "click");
+		});
+		
+		var panes = this.getPanes();
+		panes.overlayImage.appendChild(div);
+	}
+	
+	var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
+	
+	if (point) {
+		div.style.left = point.x + 'px';
+		div.style.top = point.y + 'px';
+	}
+};
 		var markers = []
 
         for (i = 0; i < results.length; i++) {
 			var markerPosition = new google.maps.LatLng(results[i]['latitude'], results[i]['longitude'])
             var marker = new google.maps.Marker({
-            	position: markerPosition,
-            	label: "results[i]['lowRate']"
+            	position: markerPosition
                 })
 	        marker.setMap(map)
+	        	var overlay = new CustomMarker(
+					markerPosition, 
+					map,
+					{}
+					);
 	        bounds.extend(markerPosition);
 			map.fitBounds(bounds);
 	       	markers.push(marker)
