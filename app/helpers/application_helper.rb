2fe12ce255ed4bbd61454f8145830b1fb3bbc4b3
Yo Wakita <wakita@gmail.com>
2015-07-15 15:25:36 -0700

    Adds more styling to show page, changing border radius.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def render_stars(value)
+    output = ''
+    if (1..5).include?(value.floor)
+      value.floor.times { output += image_tag('Star.png', :class => 'star')}
+    end
+    if value == (value.floor + 0.5) && value.to_i != 5
+      output += image_tag('HalfStar.png', :class => 'halfstar')
+    end
+    output.html_safe
+  end
 end
